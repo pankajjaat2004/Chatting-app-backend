c57d416410964be6438975aa0d2fc020520e5c22
Pankaj Kumar <pk338716@gmail.com>
2024-07-28 15:43:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const userRoutes = require('./routes/user');
 const { initSocket } = require('./socket/index');
 const helmet = require('helmet');
 const path = require('path');
-const crypto = require('crypto');
 
 const PORT = process.env.PORT || 5001;
 
@@ -16,28 +15,21 @@ mongoose.set('strictQuery', true);
 const app = express();
 require('dotenv').config();
 
-// Generate a random nonce
-const scriptNonce = crypto.randomBytes(16).toString('base64');
-
-app.use((req, res, next) => {
-  res.locals.scriptNonce = scriptNonce;
-  next();
-});
-
 // Use Helmet to set CSP and other security headers
 app.use(
   helmet({
     contentSecurityPolicy: {
       directives: {
         defaultSrc: ["'self'"],
-        scriptSrc: ["'self'", 'blob:', 'https://infimv.com', `'nonce-${scriptNonce}'`],
-        scriptSrcElem: ["'self'", 'blob:', 'https://infimv.com', `'nonce-${scriptNonce}'`],
+        scriptSrc: ["'self'", 'blob:', 'https://infimv.com'],
+        scriptSrcElem: ["'self'", 'blob:', 'https://infimv.com'],
         styleSrc: ["'self'", "'unsafe-inline'"],
         imgSrc: ["'self'", "data:"],
-        connectSrc: ["'self'", "https://chat-app-backend-a9219175d40b.herokuapp.com"],
+        connectSrc: ["'self'", "https://chatting-app-backend-6y18.onrender.com"],
+        // Add other directives as needed
       },
     },
-    crossOriginEmbedderPolicy: true,
+    crossOriginEmbedderPolicy: true, // Enable COEP
   })
 );
 
@@ -52,7 +44,8 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 const allowedOrigins = [
-  'https://chatting-clone-app-ac4b77e868b3.herokuapp.com', ""
+  'https://chatting-clone-app-ac4b77e868b3.herokuapp.com',
+  'https://chatt-app-3n7a.onrender.com'
 ];
 
 const corsOptions = {
@@ -65,9 +58,7 @@ const corsOptions = {
     }
     return callback(null, true);
   },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
-  credentials: true // Allow credentials (cookies, etc.)
+  credentials: true, // Allow credentials (cookies, etc.)
 };
 
 app.use(cors(corsOptions));
